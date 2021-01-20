9fb08724b122dd53e649f7885c1d880a0260030c
Allan Gabriel <allanalmeidagbr@gmail.com>
2021-01-20 15:00:29 -0300

    Adicionando o arquivo principal

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# projeto
+# Projeto
     SIG-Pantry - Projeto da disciplina de PROGRAMAÇÃO do curso de BSI UFRN CERES
-# desenvolvedores
+# Desenvolvedores
     Allan Gabriel Almeida Oliveira
     Joan de Azevedo Medeiros
---
+++ sigpantry.c
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///           Projeto Sistema de Controle de Despensa Doméstica             ///
+///              Developed by  @allangbr @joanmdrs - Jan, 2021              ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menuSobre(void);
+
+int main(void) {
+    menuSobre();
+    return 0;
+
+}
+
+
+void menuSobre(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///           Projeto Sistema de Controle de Despensa Doméstica             ///\n");
+    printf("///              Developed by  @allangbr @joanmdrs - Jan, 2021              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///      = = = = = Sistema de Controle de Despensa Doméstica = = = = =      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Programa exemplo utilizado na disciplina DCT1106 - Programação, para   ///\n");
+    printf("///  fins didáticos de ilustração. O programa contém os principais módulos  ///\n");
+    printf("///  e funcionalidades que serão exigidos na avaliação dos projetos a serem ///\n");
+    printf("///  desenvolvidos pelos alunos ao longo da disciplina. Serve como um guia  ///\n");
+    printf("///  de consulta e referência para o desenvolvidos dos demais projetos.     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
